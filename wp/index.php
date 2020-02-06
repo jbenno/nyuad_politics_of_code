dd6b277b309f0f854d3cbac97d5ba98efbfc5fac
Joerg Blumtritt <joerg.blumtritt@mediagnosis.de>
2020-02-06 12:11:56 +0400

    Create index.php

---
+++ wp/index.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<link rel="stylesheet" type="text/css" media="all" href="style.css" />
+		<title>Minimal Wordpress Page</title> 
+	</head>
+	<body>
+		<?php bloginfo( 'charset' ); ?>
+	</body>
+</html>
